1347860bf53b649b1da0a0cda94ebf90904d8f89
Ivan Tikhonov <kefeer@brokestream.com>
2017-10-20 10:15:27 +0100

    colortemp

--- kefeer/firefox.py
+++ kefeer/firefox.py
@@ -3,11 +3,13 @@ import json
 
 
 def is_fullscreen():
-	T=telnetlib.Telnet()
-	T.open('localhost',32000)
-	T.write('window.fullScreen')
-	r=T.read_until('}')
-	T.close()
-	return r=='{"result":true}'
+	try:
+		T=telnetlib.Telnet()
+		T.open('localhost',32000)
+		T.write('window.fullScreen')
+		r=T.read_until('}')
+		T.close()
+		return r=='{"result":true}'
 
-
+	except:
+		return False
